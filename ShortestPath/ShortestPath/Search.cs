f2c779fe6862a61ea00004274be5735a72ddeb5c
JavadEs12 <82588563+JavadEs12@users.noreply.github.com>
2022-04-26 17:42:53 +0200

    Converting static Members to locla members

--- ShortestPath/ShortestPath/Search.cs
+++ ShortestPath/ShortestPath/Search.cs
@@ -2,56 +2,57 @@
 {
     public class Search
     {
-        public static string Origin;
-        public static string Destination;
-        public static string MainDestination;
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public string MainDestination { get; set; }
+
         Dictionary<string, Node> ExtractedNodes = new Dictionary<string, Node>();
-        Heap heap;
-        public void HeapInitialization()
+        Heap heap = new Heap();
+        public void HeapInitialization(Node node1)
         {
-            heap = new Heap(Node.NetworkNodes);
+            heap = new Heap(node1.NetworkNodes);
         }
 
-        public void UpdateNodesProperties()
+        public void UpdateNodesProperties(Node node1, Arc arc1)
         {
-            HeapInitialization();
-            Node.NetworkNodes[Destination].Cost = 0;
-            Heap.Add(Node.NetworkNodes[Destination]);
-            for (int i = 0; i < Node.NetworkNodes.Count; i++)
+            HeapInitialization(node1);
+            node1.NetworkNodes[Destination].Cost = 0;
+            heap.Add(node1.NetworkNodes[Destination]);
+            for (int i = 0; i < node1.NetworkNodes.Count; i++)
             {
-                List<Arc> backwardArcs = Arc.BackArcs[Destination];
+                List<Arc> backwardArcs = arc1.BackArcs[Destination];
                 foreach (Arc arc in backwardArcs)
                 {
-                    if (!(Node.NetworkNodes[arc.Orig].Cost <= Node.NetworkNodes[arc.Dest].Cost + arc.Cost))
+                    if (!(node1.NetworkNodes[arc.Orig].Cost <= node1.NetworkNodes[arc.Dest].Cost + arc.Cost))
                     {
-                        Node.NetworkNodes[arc.Orig].Cost = arc.Cost + Node.NetworkNodes[arc.Dest].Cost;
-                        Node.NetworkNodes[arc.Orig].Successor = arc.Dest;
-                        Node node = new Node(arc.Orig, arc.Cost + Node.NetworkNodes[arc.Dest].Cost);
-                        Heap.Add(node);
+                        node1.NetworkNodes[arc.Orig].Cost = arc.Cost + node1.NetworkNodes[arc.Dest].Cost;
+                        node1.NetworkNodes[arc.Orig].Successor = arc.Dest;
+                        Node node = new Node(arc.Orig, arc.Cost + node1.NetworkNodes[arc.Dest].Cost);
+                        heap.Add(node);
                     }
                 }
-                if (!ExtractedNodes.ContainsKey(Heap.root.ID))
+                if (!ExtractedNodes.ContainsKey(heap.root.ID))
                 {
-                    ExtractedNodes.Add(Node.NetworkNodes[Heap.root.ID].ID, Node.NetworkNodes[Heap.root.ID]);
+                    ExtractedNodes.Add(node1.NetworkNodes[heap.root.ID].ID, node1.NetworkNodes[heap.root.ID]);
                 }
 
-                if (ExtractedNodes.Count == Node.NetworkNodes.Count) { break; }
+                if (ExtractedNodes.Count == node1.NetworkNodes.Count) { break; }
                 while (true)
                 {
-                    Node des = Heap.Remove();
+                    Node des = heap.Remove();
                     if (!ExtractedNodes.ContainsKey(des.ID))
                     {
-                        Destination = Node.NetworkNodes[des.ID].ID;
+                        Destination = node1.NetworkNodes[des.ID].ID;
                         break;
                     }
                 }
             }
         }
-        public void ShortestPath()
+        public void ShortestPath(Node node1, Arc arc1)
         {
             try
             {
-                UpdateNodesProperties();
+                UpdateNodesProperties(node1, arc1);
                 List<string> ShortestPath = new List<string>();
                 ShortestPath.Add(Origin);
 
@@ -77,4 +78,3 @@
         }
     }
 }
-
