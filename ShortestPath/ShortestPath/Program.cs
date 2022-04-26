a8dfbd58da03ead8a57fd9a0661a46a701277530
JavadEs12 <82588563+JavadEs12@users.noreply.github.com>
2022-04-26 23:44:03 +0200

    Converting static to non-static members

--- ShortestPath/ShortestPath/Program.cs
+++ ShortestPath/ShortestPath/Program.cs
@@ -6,41 +6,42 @@ namespace ShortestPath
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Shortest Path");
-            InputData();
-            Implementation();
+            Arc arc1 = new Arc();
+            Search search1 = new Search();
+
+            Program program = new Program();
+            program.InputData(search1);
+            Node node1 = new Node();
+            Implementation(arc1, search1, node1);
         }
-        static void Implementation()
+        static void Implementation(Arc arc1, Search search1, Node node1)
         {
-            Arc.arcs = JsonConvert.DeserializeObject<List<Arc>>(File.ReadAllText("D:/ShortestPath/ShortestPathInput.txt"));
+            arc1.arcs = JsonConvert.DeserializeObject<List<Arc>>(File.ReadAllText("D:/ShortestPath/ShortestPathInput.txt"));
+            arc1.NodeAssignment(node1);
 
-            Arc obj = new Arc();
-            obj.NodeAssignment();
-
-            Search obj2 = new Search();
-            obj2.ShortestPath();
+            search1.ShortestPath(node1, arc1);
         }
 
-        static void InputData()
+        private void InputData(Search search1)
         {
             Console.WriteLine("Please choose your Origin-Destination couple\nYou are allowed to choose numbers between 1-6");
-            Search.Origin = InputAccuracy();
-            Search.Destination = InputAccuracy();
-            Search.MainDestination = Search.Destination;
+            search1.Origin = InputAccuracy(search1);
+            search1.Destination = InputAccuracy(search1);
+            search1.MainDestination = search1.Destination;
         }
 
-        static string InputAccuracy()
+        private string InputAccuracy(Search search1)
         {
             while (true)
             {
                 int input = Int16.Parse(Console.ReadLine());
-                if (Search.Origin == null && input >= 1 && input <= 6)
+                if (search1.Origin == null && input >= 1 && input <= 6)
                 {
                     return input.ToString();
                 }
-                else if (Search.Origin != null && input >= 1 && input <= 6)
+                else if (search1.Origin != null && input >= 1 && input <= 6)
                 {
-                    if (input.ToString() == Search.Origin)
+                    if (input.ToString() == search1.Origin)
                     {
                         Console.WriteLine("Entered number is repeated, please try another number for destination");
                     }
