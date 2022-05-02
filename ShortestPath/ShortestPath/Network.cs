62c7fe55aa8fc789f270da888dee1759368226ea
Javad Eslamibabaheidari <82588563+JavadEs12@users.noreply.github.com>
2022-05-02 11:19:05 +0200

    Add Network class

---
+++ ShortestPath/ShortestPath/Network.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortestPath
+{
+    public class Network
+    {
+        public Dictionary<string,Arc> NetworkArcs = new Dictionary<string, Arc>();
+        public Dictionary<string, Node> NetworkNodes = new Dictionary<string, Node>();
+        public Dictionary<string, List<Arc>> NetworkNodesBackArcs = new Dictionary<string, List<Arc>>();  
+        public Dictionary<string, Node> ExtractedNodes = new Dictionary<string, Node>();
+        public Heap heap = new Heap();
+    }
+}
