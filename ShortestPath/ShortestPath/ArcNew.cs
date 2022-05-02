526823307756c9a41abc26b3acecddeef4a7b95d
Javad Eslamibabaheidari <82588563+JavadEs12@users.noreply.github.com>
2022-05-02 11:08:38 +0200

    Add "ArcNew" which is a new arc class

---
+++ ShortestPath/ShortestPath/ArcNew.cs
@@ -0,0 +1,10 @@
+﻿namespace ShortestPath
+{
+    public class ArcNew
+    {
+        public string Idno { get; set; }
+        public string Orig { get; set; }
+        public string Dest { get; set; }
+        public double Cost { get; set; }
+    }
+}
