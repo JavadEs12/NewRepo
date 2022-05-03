8396967ea4c316423cf746c192f05b3254846642
Javad Eslamibabaheidari <82588563+JavadEs12@users.noreply.github.com>
2022-05-03 13:00:49 +0200

    Add ArrayHeap AddNewNode method

--- ShortestPath/ShortestPath/ArrayHeap.cs
+++ ShortestPath/ShortestPath/ArrayHeap.cs
@@ -11,15 +11,14 @@ namespace ShortestPath
     {
         public ArrayList Heap = new ArrayList();
         private Node Root;
-        private Node Pointer;
         private Node Child;
         private Node Parent;
         private int count;
 
-        public ArrayHeap(Dictionary<string,Node> nodes)
+        public ArrayHeap(Dictionary<string, Node> nodes)
         {
             count = 0;
-            foreach (KeyValuePair<string,Node> node in nodes)
+            foreach (KeyValuePair<string, Node> node in nodes)
             {
                 AddNewNode(node.Value);
             }
@@ -27,19 +26,33 @@ namespace ShortestPath
         public void AddNewNode(Node node)
         {
             Heap.Add(node);
-            count++;    
-            perculateUp();
+            count++;
+            PerculateUp();
         }
 
-        private void perculateUp()
+        private void PerculateUp()
         {
-            if(Heap.Count == 0) { return; }
-            
+            if (Heap.Count == 0) { return; }
+
             else
             {
-                
+                int pointer = count - 1;
+                while (true)
+                {
+                    Child = (Node)Heap[pointer];
+                    Parent = (Node)Heap[Math.Abs((pointer) / 2)]; //Maybe need to round down 
+                    Node comparer;
+                    if (Child.Cost <= Parent.Cost)
+                    {
+                        comparer = Child;
+                        Heap[pointer] = Heap[Math.Abs((pointer) / 2)];
+                        Heap[Math.Abs((pointer) / 2)] = comparer;
+                    }
+                    pointer = Math.Abs((pointer) / 2);
+                    if(pointer == 0) { break; }
+                }
             }
-            
+
         }
     }
 }
