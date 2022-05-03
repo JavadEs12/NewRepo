3b181b1a00f59249c1472176dfc7cddd3cb01410
Javad Eslamibabaheidari <82588563+JavadEs12@users.noreply.github.com>
2022-05-03 17:49:03 +0200

    Add InterfaceExample

---
+++ ShortestPath/ShortestPath/InterfaceExample.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortestPath
+{
+    public interface ISum
+    {
+        public double Sum(double left, double right);
+    }
+
+    internal class Sum : ISum
+    {
+        double ISum.Sum(double left, double right)
+        {
+            return left + right;
+        }
+    }
+
+    internal class AnotherSum : ISum
+    {
+        public double Sum(double left, double right)
+        {
+            return left + right;
+        }
+    }
+
+    public class SumClient
+    {
+        public ISum Adder { get; set; }
+
+        public void DoWork()
+        {
+            var value = Adder.Sum(1, 1);
+        }
+    }
+
+    public static class SumClientBuilder
+    {
+        public static SumClient Build(int option)
+        {
+            if (option == 0)
+            {
+                return new SumClient { Adder = new Sum() };
+            }
+            else
+            {
+                return new SumClient { Adder = new AnotherSum() };
+            }
+        }
+    }
+}
