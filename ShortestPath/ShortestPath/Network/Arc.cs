94a869460debecbbeb7ac3eaec7b34868726a1ff
Javad Eslamibabaheidari <eslamibabaheidari.1871591@studenti.uniroma1.it>
2022-05-06 09:56:08 +0200

    Create Network folder to put Network related stuff there

---
+++ ShortestPath/ShortestPath/Network/Arc.cs
@@ -0,0 +1,10 @@
+﻿namespace ShortestPath
+{
+    public class Arc
+    {
+        public string Idno { get; set; }
+        public string Orig { get; set; }
+        public string Dest { get; set; }
+        public double Cost { get; set; }
+    }
+}
---
+++ ShortestPath/ShortestPath/Network/Node.cs
@@ -0,0 +1,23 @@
+﻿namespace ShortestPath
+{
+    public class Node
+
+    {
+        public string ID { get; set; }
+        public double Cost { get; set; }
+        public string Successor { get; set; }
+
+        public Node(string ID, double Cost, string Successor)
+        {
+            this.ID = ID;
+            this.Cost = Cost;
+            this.Successor = Successor;
+        }
+
+        public Node(string ID, double Cost)
+        {
+            this.ID = ID;
+            this.Cost = Cost;
+        }
+    }
+}
