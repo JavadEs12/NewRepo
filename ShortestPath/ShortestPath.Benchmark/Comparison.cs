9b7bc8b1fb01925f628d3cdef460387060c3adc7
Javad Eslamibabaheidari <82588563+JavadEs12@users.noreply.github.com>
2022-05-09 11:31:54 +0200

    Add Benchmark Project

---
+++ ShortestPath/ShortestPath.Benchmark/Comparison.cs
@@ -0,0 +1,37 @@
+﻿using BenchmarkDotNet.Attributes;
+using Newtonsoft.Json;
+using ShortestPath;
+
+namespace Benchmark
+{
+    [MemoryDiagnoser]
+    [Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class Comparison
+    {
+        public Network network;
+        public Heap heap = new Heap();
+        ArrayHeap arrayHeap = new ArrayHeap();
+        public void ReadJson()
+        {
+            var input = File.ReadAllText("ShortestPathInput.txt");
+            var deserializer = new JsonDeserializer();
+            List<Arc> arcs = deserializer.Deserialize(input);
+
+            var networkBuilder = new NetworkBuilder();
+            network = networkBuilder.BuildNetwork(arcs);
+        }
+        [Benchmark]
+        public void ArrayHeapBenchmark()
+        {
+            ReadJson();
+            arrayHeap = new ArrayHeap(network.Nodes);
+        }
+        [Benchmark]
+        public void HeapBenchmark()
+        {
+            ReadJson();
+            heap = new Heap(network.Nodes);
+        }
+    }
+}
